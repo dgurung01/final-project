2939032b9978e0cb0e69971a9fbe3123088cacb1
IndimaDias <indimad@yahoo.com>
2019-08-30 23:32:03 -0400

    display events as icons, modal when event clicked

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,23 +3,19 @@ import axios from "axios";
 // Export an object containing methods we'll use for accessing the Dog.Ceo API
 
 export default {
-  getRandomDog: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  },
-  getDogsOfBreed: function(breed) {
-    return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-  },
-  getBaseBreedsList: function() {
-    return axios.get("https://dog.ceo/api/breeds/list");
-  },
-  
+
   saveEvent : function(eventData) {
   
     return axios.post("/api/events",eventData);
   },
 
-  getEvents : function(){
+  getEvents : function(id){
+    if (id) {    
+      return axios.get("/api/events/" + id);
+    }
+    else {    
+      return  axios.get("/api/events" );
+    }
     
-    return  axios.get("/api/events");
   }
 };
